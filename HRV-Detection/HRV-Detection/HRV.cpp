659ef60eb7ff127065941f80ce9d8841557cc041
Shubhankar-creation <shubhankarsharma0135@gmail.com>
2021-10-27 12:37:29 +0530

    Created storeData() which stores all the raw data in a vector.

--- HRV-Detection/HRV-Detection/HRV.cpp
+++ HRV-Detection/HRV-Detection/HRV.cpp
@@ -3,7 +3,24 @@
 #include <string>
 #include <vector>
 
-class HRV
-{
+using namespace std;
 
-};
+fstream rawData;
+vector<float> y;
+
+void storeData()
+{
+	while (rawData.good())
+	{
+		string line;
+		getline(rawData, line, '\n');
+		y.push_back(stof(line));
+	}
+	cout << y.size();
+}
+int main()
+{
+	rawData.open("Dataset/ecg-1.csv");
+	storeData();
+	return 0;
+}
